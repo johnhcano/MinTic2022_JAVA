3477c6adae6c5b18b70f23d622dc77be64af7e27
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-06-30 11:45:04 -0500

    clase1

--- P2/clase1/src/App.java
+++ P2/clase1/src/App.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.print("Digite su nombre: ");
+        
         //capturar info por teclado
         Scanner sc = new Scanner(System.in);
-        String resultado = sc.next(); //obtenemos del teclado el nombre
+        
+        System.out.print("Digite su nombre: ");
+        String resultado = sc.nextLine(); //obtenemos del teclado el nombre
         System.out.println(saludo(resultado));
+
+        System.out.print("Digite un número: ");
+        int num = sc.nextInt();
+        System.out.println(numeros(num));
+
+        System.out.print("Digite los grados centigrados: ");
+        double g = sc.nextDouble();
+        System.out.println("La temperatura(F) es: " + grados(g));
+
+        System.out.print("Digite un número: ");
+        int num1 = sc.nextInt();
+        System.out.println(operadorCondicional(num1));
+
     }
 
     //Metodo saludo
@@ -15,4 +30,26 @@ public class App {
         return "Hola " + nombre + "!, Cómo estás?";
     }
 
+    //Metodo doble o triple
+    public static String numeros(int n){
+        return n + "=> doble es " + (n*2) + ", triple es " + (n*3); 
+    }
+
+    //Metodo grados
+    public static double grados(double gc){
+        double f = 32 + (9 * (gc/5));
+        return f;
+    }
+    
+    //Metodo operador condicional
+    public static String operadorCondicional(int n){
+        return n%2==0?n+" es Par":n+" es Impar";
+
+        /*String respuesta = "";
+        if(n%2 == 0){
+            respuesta = n + "es Par";
+        }else{
+            respuesta = n + "es Impar";
+        }*/
+    }
 }
