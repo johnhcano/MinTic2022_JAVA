fe6f10c595751f3e22284fcce54ef15a35f8ed33
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-06-30 22:56:20 -0500

    iniciar

--- P62/clase01/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int x = 1;
-        System.out.println(x++);
-        System.out.println(x);
-    }
-}
