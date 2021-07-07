24a7e7726025159736ae6acc1e0776abc64d17c7
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-07-07 09:29:08 -0500

    cliclo2

---
+++ P2/clase2/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,23 @@
+package co.edu.utp.misiontic2022.c2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Persona objPersona1 = new Persona(12345,"Jhon","Cano","M");
+        Persona objPersona2 = new Persona(10001,"Harvin","Cano","M");
+
+        System.out.println(objPersona1.getIdentificacion());
+        System.out.println(objPersona2.getIdentificacion());
+        System.out.println(objPersona2);
+        System.out.println(objPersona2.getNombre());
+        objPersona2.setIdentificacion(99887);
+        objPersona2.setNombre("Ferney");
+        System.out.println(objPersona2.getNombre());
+        System.out.println(objPersona2);
+    }
+}
