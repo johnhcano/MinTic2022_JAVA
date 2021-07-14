00f5457e1cd1aef38b0767669e192576c5404f74
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-07-14 17:44:08 -0500

    ciclo2

--- P32/clase3/src/main/java/co/edu/utp/misiontic2022/c3/App.java
+++ P32/clase3/src/main/java/co/edu/utp/misiontic2022/c3/App.java
@@ -14,11 +14,17 @@ public class App
         Persona objPersona1 = new Empleado("Byron", 20, 10546.32);
         Persona objPersona2 = new Cliente("Javier", 30, "(032) 446 23 45");
 
+        //Polimorfismo: Capacidad que tiene un método de comportarse de diferentes maneras, según el objeto que lo llame. ej: mostrar()
         objPersona1.mostrar();
         objPersona2.mostrar();
 
         System.out.println(objPersona1);
         System.out.println(objPersona2);
+
+        //Se crea el objeto de Empresa para validar la relación entre Empresa y Empleado
+        //Composición --> Relación fuerte --> Se hace a través del atributo de la clase.
+        Empresa objEmpresa = new Empresa("MisiónTIC2022", (Empleado)objPersona1);
+        System.out.println(objEmpresa);
         
     }
 }
