aebb9a76611463a75c3a16d3332fea575c8e7fbf
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-04 18:10:42 -0500

    ciclo2

---
+++ P32/clase9/src/Converter.java
@@ -0,0 +1,24 @@
+public class Converter {
+    
+    private String document;
+
+    public Converter(String document) {
+        this.document = document;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public String makeLine(){
+        return "";
+    }
+
+    public String makeParagraph(){
+        return "";
+    }
+
+    public String makeTable(){
+        return "";
+    }
+}
