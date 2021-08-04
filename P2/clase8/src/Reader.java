0bd4f3f0a0f9d88e603b9d674befde3139b9e071
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-04 12:44:45 -0500

    ciclo2

--- P2/clase8/src/Reader.java
+++ P2/clase8/src/Reader.java
@@ -8,7 +8,19 @@ public class Reader {
         this.converter = converter;
     }
 
+    public String getElement(){
+        return element;
+    }
+
     public void parseInput(){
-        
+        if(element.equals("1")){
+            System.out.println(converter.toString());
+        }else if(element.equals("2")){
+            System.out.println(converter.toString());
+        }else if(element.equals("3")){
+            System.out.println(converter.toString());
+        }else{
+            System.out.println("Elemento no existe!!");
+        }
     }
 }
