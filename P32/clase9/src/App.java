0fdd6945b0109fb4d2838ff87bf7a7f1e7e8080a
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-05 14:44:30 -0500

    ciclo2

--- P32/clase9/src/App.java
+++ P32/clase9/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
         
+        Converter objConverter = new ASCIIConverter("Hoja de Vida");
+
+        Reader objReader = new Reader("LINE", objConverter);
+
+        System.out.println(objReader.parseInput());
+        
     }
 }
