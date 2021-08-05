669f942ca4b488f6cc83496e26d4f4c51ee76c1b
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-05 15:30:43 -0500

    ciclo2

--- P32/clase9/src/App.java
+++ P32/clase9/src/App.java
@@ -12,13 +12,24 @@ public class App {
         ciudades[1] = "Bogotá";
         ciudades[2] = "Barranquilla";
         for (int i = 0; i < ciudades.length; i++) {
-            System.out.println(ciudades[i]);
+            System.out.println("Arreglo -> " + ciudades[i]);
         }
 
         ArrayList<String> ciudades1 = new ArrayList<>();
         ciudades1.add("Cali");
         ciudades1.add("Bogotá");
         ciudades1.add("Barranquilla");
+        for (int i = 0; i < ciudades1.size(); i++) {
+            System.out.println("ArrayList -> " + ciudades1.get(i));
+        }
+        
+        ciudades1.forEach((c) -> {
+            System.out.println("forEach -> " + c);
+        });
+
+        for(String c: ciudades1){
+            System.out.println("for: -> " + c);
+        }
 
     }
 }
