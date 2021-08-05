b36baaa88d6d3900ea2337fbb50aa7d3f93f89d9
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-05 08:26:23 -0500

    ciclo2

--- P62/clase8/src/App.java
+++ P62/clase8/src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Converter libro = new PDFConverter("El Perfume");
+
+        Reader reader = new Reader("1", libro);
+
+        System.out.println(reader.parseInput());
+
+
     }
 }
