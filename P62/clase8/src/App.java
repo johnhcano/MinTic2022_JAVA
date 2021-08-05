3b4258303f41009f6160ac04a6702f987658abb4
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-05 09:33:02 -0500

    ciclo2

--- P62/clase8/src/App.java
+++ P62/clase8/src/App.java
@@ -5,22 +5,31 @@ public class App {
         
         Converter libro = new PDFConverter("El Perfume");
         Reader reader = new Reader("1", libro);
-        System.out.println(reader.parseInput());
+        System.out.println("reader " + reader.parseInput());
 
-        System.out.println("----------------");
-        //ArrayList<Converter> documentos =  new ArrayList<>();
+        ArrayList<Converter> documentos =  new ArrayList<>();
+        Converter excel = new ASCIIConverter("Nómina");
+        Converter word = new PostScriptConverter("Carta Saludo");
+        Converter ppt = new PDFConverter("Presentación");
+        documentos.add(excel);
+        documentos.add(word);
+        documentos.add(ppt);
 
+        Reader lector = new Reader("2", documentos.get(1));
+        System.out.println("lector " + lector.parseInput());
+
+        System.out.println("------------Explicación ArrayList VS Array----");
         ArrayList<String> nombres = new ArrayList<>();
         nombres.add("AL -> Jhon");
         nombres.add("AL -> Byron");
         nombres.add("AL -> Harvin");
+        
         for(int i = 0; i < nombres.size(); i++){
             System.out.println(nombres.get(i));
         }
         nombres.forEach((names) -> {
             System.out.println(names);
         });
-
         for(String names: nombres){
             System.out.println(names);
         }
