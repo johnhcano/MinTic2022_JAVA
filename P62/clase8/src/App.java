0b23e847065f3cd816475eae692a9b2e5d229c50
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-05 08:55:57 -0500

    ciclo2

--- P62/clase8/src/App.java
+++ P62/clase8/src/App.java
@@ -1,11 +1,40 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         
         Converter libro = new PDFConverter("El Perfume");
-
         Reader reader = new Reader("1", libro);
-
         System.out.println(reader.parseInput());
+
+        System.out.println("----------------");
+        //ArrayList<Converter> documentos =  new ArrayList<>();
+
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("AL -> Jhon");
+        nombres.add("AL -> Byron");
+        nombres.add("AL -> Harvin");
+        for(int i = 0; i < nombres.size(); i++){
+            System.out.println(nombres.get(i));
+        }
+        nombres.forEach((names) -> {
+            System.out.println(names);
+        });
+
+        for(String names: nombres){
+            System.out.println(names);
+        }
+
+        System.out.println("----------------");
+
+        String[] personas = new String[3];
+        personas[0] = "A -> Jhon";
+        personas[1] = "A -> Byron";
+        personas[2] = "A -> Harvin";
+        for(int i = 0; i < personas.length; i++){
+            System.out.println(personas[i]);
+        }
+
 
     }
 }
