0b494e94002eac8438908108f447944f5428571d
Jhon Cano <jhoncano@MacBook-Pro-de-jhoncano.local>
2021-08-11 07:15:12 -0500

    ciclo2

---
+++ P62/Reto4_P62_plantilla/src/main/java/view/VistaRequerimientosReto4.java
@@ -0,0 +1,33 @@
+package view;
+
+import controller.ControladorRequerimientosReto4;
+//import model.vo.LiderCiudad;
+//import model.vo.SumaProveedor;
+import model.vo.ProyectoCiudad;
+
+import java.sql.SQLException;
+//import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class VistaRequerimientosReto4 {
+
+    public static ControladorRequerimientosReto4 controlador = new ControladorRequerimientosReto4();
+
+    public static void requerimiento1() {
+
+
+    }
+
+    public static void requerimiento3() {
+
+        
+
+    }
+
+    public static void requerimiento4() {
+
+        
+
+    }
+
+}
